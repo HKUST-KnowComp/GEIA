817dcf45bdac88877097ae826fce6aa7ddd34fe9
teapotliid <hlibt@connect.ust.hk>
2023-05-04 17:05:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import logging
 
-processed_persona = '/home/hlibt/embed_rev/personachat/processed_persona'
-abcd_path = '/home/hlibt/embed_rev/data/abcd/abcd_v1.1.json'
+processed_persona = 'data/personachat/processed_persona'
+abcd_path = 'data/abcd/abcd_v1.1.json'
 
 #Don't need to modify
 logging_level = logging.INFO
