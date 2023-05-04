cccd486cbc0265ed92e8fc513211601c850c8444
teapotliid <hlibt@connect.ust.hk>
2023-05-04 16:45:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import logging
-#persona_path = '/raid0/hlibt/data/personachat/personachat_self_original.json'
-#personachat_path = '/home/hlibt/ParlAI/data/Persona-Chat/personachat/train_self_original.txt'
-dnli_path = '/data/hlibt/acl2022/mycodes/data/dnli/dnli/dialogue_nli'
+
 processed_persona = '/home/hlibt/embed_rev/personachat/processed_persona'
 abcd_path = '/home/hlibt/embed_rev/data/abcd/abcd_v1.1.json'
 
